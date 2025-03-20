9b4ea5052c6ca069ff09088b2bca3272a2553996
nishtha <pnish015@gmail.com>
2025-03-20 17:19:25 +0530

    update category

--- resources/views/admin/showCategory.blade.php
+++ resources/views/admin/showCategory.blade.php
@@ -146,8 +146,10 @@
         <table class="table table-striped table-bordered">
             <thead style="background-color: #616b6b;">
                 <tr>
-                    <th colspan="3" align="center">
-                        <h5>Expense Categories</h5>
+                <th colspan="3">
+                        <center>
+                            <h5>Expense Categories</h5>
+                        </center>
                     </th>
                 </tr>
                 <tr>
